61b2b2a49e0e9487dbd23c8cc909d0ef823d6362
Rob Anderson <robandpdx@github.com>
2022-09-26 15:35:46 -0700

    fix branch and tag names

--- push_branches.sh
+++ push_branches.sh
@@ -3,8 +3,8 @@
 set -u
 set -e
 cd $GIT_REPO
-for REF in $(git for-each-ref --format='%(refname)' refs/remotes/svn/ | grep -v /tags/); do
-    BRANCH_NAME=${REF#refs/remotes/origin/}
+for REF in $(git for-each-ref --format='%(refname)' refs/remotes/svn/ | grep -v /tags/ | grep -v @); do
+    BRANCH_NAME=${REF#refs/remotes/svn/}
     if [ "$BRANCH_NAME" != "trunk" ]; then
         echo "Push branch: $BRANCH_NAME"
         git push -u origin "$REF":refs/heads/$BRANCH_NAME
--- push_tags.sh
+++ push_tags.sh
@@ -4,7 +4,7 @@ set -u
 set -e
 cd $GIT_REPO
 for REF in $(git for-each-ref --format='%(refname)' refs/remotes/svn/ | grep /tags/); do
-    TAG_NAME=${REF#refs/remotes/origin/tags/}
+    TAG_NAME=${REF#refs/remotes/svn/tags/}
     echo "Push tag: $TAG_NAME"
     git push -u origin "$REF":refs/tags/$TAG_NAME
 done
